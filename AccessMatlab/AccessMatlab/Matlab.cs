c22ebd4d25de55ce0d346aa175ebfc18db191517
Saurabh V. Palaspagar <saurabh.palaspagar@outlook.com>
2016-12-01 21:12:08 -0500

    Dll and master files

---
+++ AccessMatlab/AccessMatlab/Matlab.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessMatlab
+{
+    public class Matlab
+    {
+        public static int callFunction(int number) {
+
+            return number + number;
+        }
+    }
+}
---
+++ VehicleArrivalModel/VehicleArrivalModel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccessMatlab;
+
+namespace VehicleArrivalModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Matlab.callFunction(2));
+            Console.ReadLine();
+            
+        }
+    }
+}
